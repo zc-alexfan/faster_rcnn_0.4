bf810adad4ea639dafc7a747e8997dc35a21bfc0
Alex <fan@cs.ubc.ca>
2019-03-03 11:38:30 -0800

    fixed bug in import future

--- trainval_net.py
+++ trainval_net.py
@@ -2,7 +2,10 @@
 # Pytorch multi-GPU Faster R-CNN
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
-# -------------------------------------------------------- from __future__ import absolute_import from __future__ import division
+# -------------------------------------------------------- 
+
+from __future__ import absolute_
+import from __future__ import division
 from __future__ import print_function
 
 import _init_paths
